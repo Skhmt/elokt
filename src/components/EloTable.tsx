3f9283b0c0438ad6a2643557775fcd21c39d2c55
Skhmt <Skhmt@users.noreply.github.com>
2024-08-27 15:27:14 -0700

    Update EloTable.tsx

--- src/components/EloTable.tsx
+++ src/components/EloTable.tsx
@@ -5,40 +5,35 @@ import { useAtom } from 'jotai'
 import { eloData } from '../store/store'
 import supabase from '../lib/supabase'
 import './EloTable.css'
+import { useEffect } from 'react'
 
 // https://datatables.net/blog/2024/react
 DataTable.use(DT)
 
-let startedFetch = false
-
 const pageLength = 25
 
-async function fetchData() {
-	startedFetch = true
-
-	const [, setTableData] = useAtom(eloData) // eslint-disable-line
-
-	const { data, error } = await supabase
-		.from('elo')
-		.select('first_name, last_name, elo')
-
-	const arrData = data?.map(row => {
-		return [row.first_name || '', row.last_name || '', row.elo]
-	})
-
-	if (error) console.error(error)
-
-	// console.log(data)
-	// console.log(arrData)
-
-	// @ts-expect-error: ts(2345)
-	setTableData(arrData)
-}
-
 export function EloTable() {
-	const [tableData] = useAtom(eloData)
+	const [tableData, setTableData] = useAtom(eloData)
 
-	if (!startedFetch) fetchData()
+	useEffect(() => {
+		(async () => {
+			const { data, error } = await supabase
+				.from('elo')
+				.select('first_name, last_name, elo')
+
+			const arrData = data?.map(row => {
+				return [row.first_name || '', row.last_name || '', row.elo]
+			})
+
+			if (error) console.error(error)
+
+			// console.log(data)
+			// console.log(arrData)
+
+			// @ts-expect-error: ts(2345)
+			setTableData(arrData)
+		})()
+	}, [setTableData])
 
 	return (
 		<DataTable data={tableData} options={{
